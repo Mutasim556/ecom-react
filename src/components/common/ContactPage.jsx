914bad1a790a45363f00ebc3d18e3548949486b3
Md Mutasim Naib <mutasim@inceptapharma.com>
2023-05-31 17:16:58 +0600

    Change

--- src/components/common/ContactPage.jsx
+++ src/components/common/ContactPage.jsx
@@ -1,7 +1,67 @@
+import axios from 'axios';
 import React, { Component, Fragment } from 'react'
-import { Container,Row,Col, Form, Button } from 'react-bootstrap'
+import { Container,Row,Col, Form, Button,Alert } from 'react-bootstrap'
+import appURL from '../api/appURL';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export class ContactPage extends Component {
+  constructor(){
+    super();
+    this.state ={
+      name : '',
+      email : '',
+      message : '',
+      show : false,
+    }
+  }
+  formValidation = () =>{
+    if(this.state.name===''){
+      alert('Faka')
+    }else if(this.state.email===''){
+      alert('Faka')
+    }else if(this.state.message===''){
+      alert('Faka')
+    }else{
+      return true;
+    }
+  }
+  submitForm = (e)=>{
+    e.preventDefault();
+    if(this.formValidation()){
+        var formData = new FormData();
+        var btndata = document.getElementById('submitBtn');
+        var onboardForm = document.getElementById('onboardForm');
+        btndata.innerHTML='Sending........';
+        formData.append("name",this.state.name);
+        formData.append("email",this.state.email);
+        formData.append("message",this.state.message);
+        axios.post(appURL.BaseURL+'/contact',{
+          name : this.state.name,
+          email : this.state.email,
+          message : this.state.message
+        }).then((res)=>{
+          // this.setState({
+          //   insert : 'successfull'
+          // });
+          toast('🦄 Message Send Successfully');
+          btndata.innerHTML='Send';
+          onboardForm.reset();
+        }).catch((err)=>{
+
+        })
+    }
+  }
   render() {
+    var noti = () =>{
+      if(this.state.insert){
+        return(
+          <Alert variant="success" onClose={(e) =>{this.setState({show:false,insert:''})} } dismissible>
+          <Alert.Heading>Your message send successfully</Alert.Heading>
+           
+          </Alert>
+        );
+      }
+    }
     return (
       <Fragment>
         <Container>
@@ -9,12 +69,13 @@ export class ContactPage extends Component {
             <Col className='p-2 shadow-sm' xl="12" lg="12" md="12" xs="12" sm="12">
                 <Row className='text-center'>
                   <Col className='d-flex justify-content-center' xl="6" lg="6" md="6" sm="12" xs="12">
-                      <Form className='onboardForm'>
+                      <Form onSubmit={this.submitForm} className='onboardForm' id='onboardForm'>
                           <h3  className='section-title-login'>Contact with us</h3>
-                          <input type="text" className='form-control m-2' placeholder='Enter your mobile number'/>
-                          <input type="text" className='form-control m-2' placeholder='Enter your email'/>
-                          <textarea rows="7" className='form-control m-2' placeholder='Enter your message'/>
-                          <Button className='btn btn primary m-2 site-btn-login'>Send</Button>
+                          {noti()}
+                          <input type="text" className='form-control m-2' placeholder='Enter your name' onChange={(e)=>{this.setState({name:e.target.value})}}/>
+                          <input type="text" className='form-control m-2' placeholder='Enter your email' onChange={(e)=>{this.setState({email:e.target.value})}}/>
+                          <textarea rows="7" className='form-control m-2' placeholder='Enter your message' onChange={(e)=>{this.setState({message:e.target.value})}}/>
+                          <Button type='submit' id='submitBtn' className='btn btn primary m-2 site-btn-login'>Send</Button>
                       </Form>
                   </Col>
                   <Col className='p-2 m-0 Desktop'  xl="6" lg="6" md="6" sm="12" xs="12">
@@ -25,6 +86,18 @@ export class ContactPage extends Component {
             </Col>
           </Row>
         </Container>
+        <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        />
       </Fragment>
     )
   }
